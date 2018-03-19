883e3cb54b40264d47acc59c5cd40680617adfff
Alexandra da Silva <alesilva241@gmail.com>
2018-03-19 09:51:38 -0300

    basic webscrapper

--- src/webscrapper.py
+++ src/webscrapper.py
@@ -1,10 +1,37 @@
-from lxml import html
+import lxml.html
+import yaml
+from lxml import etree
 import requests
 
-response = requests.get('https://s3-eu-west-1.amazonaws.com/legalstart/thumbscraper_input_tampered.hiring-env.json')
+config = yaml.load(open('/Users/apereira/Downloads/TesteHekima/Legal/src/systemConfig.yaml'))
+responseJson = requests.get(config['json_info']['input_json'])
+jsonData = responseJson.json()
+urlBase = config['url_info']['base_url']
+user = config['credentials']['user']
+password = config['credentials']['password']
 
-data = response.json()
+def getHtmlFile(url_name, user, password):
+    htmlFile = requests.get(url_name, auth=(user, password)) 
+    return htmlFile
 
-#test=html.xpath(data['0']['xpath_test_query'])
-
-print(data['0']['xpath_test_query'])
+for i in range(len(jsonData)):
+    if i == 0:
+        nextPage = jsonData[str(i)]['next_page_expected']
+        responseHtml = getHtmlFile(urlBase, user, password)
+        htmlText = lxml.html.fromstring(responseHtml.content)
+        parsedContent = htmlText.xpath(jsonData[str(i)]['xpath_test_query'])
+        nextLink = htmlText.xpath(jsonData[str(i)]['xpath_button_to_click'])
+        if validParser(parsedContent, jsonData[str(i)]['xpath_test_result']):
+            print(outputMessage(True, i))
+        else:
+            print(outputMessage(False, i))
+    else:
+        responseHtml = getHtmlFile(urlBase+nextLink[0].get('href'), user, password)
+        htmlText = lxml.html.fromstring(responseHtml.content)
+        parsedContent = htmlText.xpath(jsonData[str(nextPage)]['xpath_test_query'])
+        nextLink = htmlText.xpath(jsonData[str(nextPage)]['xpath_button_to_click'])
+        if validParser(parsedContent, jsonData[str(nextPage)]['xpath_test_result']):
+            print(outputMessage(True, i))
+        else:
+            print(outputMessage(False, i))
+        nextPage = jsonData[str(nextPage)]['next_page_expected']
