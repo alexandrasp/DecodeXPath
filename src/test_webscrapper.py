ca750ee229b935e295de1b3c38d5ea8202d434d8
Alexandra da Silva <alesilva241@gmail.com>
2018-03-19 10:38:20 -0300

    update gitignore

--- src/test_webscrapper.py
+++ src/test_webscrapper.py
@@ -6,6 +6,10 @@ class TestWepscrapper(unittest.TestCase):
     def test_outputMessage_false(self):       
         result = webscrapper.outputMessage(False, 1)
         self.assertEqual(result, "ALERT - Can’t move to page 2: page 1 link has been malevolently tampered with!!")
+    
+    def test_outputMessage_true(self):       
+        result = webscrapper.outputMessage(True, 1)
+        self.assertEqual(result, "Move to page 2")
 
 if __name__ == '__main__':
     unittest.main()
